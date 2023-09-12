02cbf8c5abc412cc04acef0d35d047170a4f1099
MIMIMINISAIA <emillybeatrizamoroso@Gmail.com>
2023-09-12 15:28:34 -0300

    exercicio 6

---
+++ loja.ts
@@ -0,0 +1,110 @@
+class Produto{
+    nome: string;
+    preco: number;
+   
+
+    constructor(nome: string, preco: number){
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
+
+class carrinhoDeCompra{
+     lista: Produto[] = [];
+    
+    constructor(){
+
+    }
+    
+    adicionarProduto(produto: Produto){
+        this.lista.push(produto);
+
+    }
+
+    remover(produto: Produto){
+
+        this.lista = this.lista.filter((item) => item.nome !== produto.nome);
+        return "produto removido"
+
+    }
+
+    Calcular(){
+        let soma =  0;
+        this.lista.forEach( item =>{soma = soma + item.preco});
+
+        //for(let i=0; i< this.lista.length; i++){
+        //   soma = soma + this.lista[i].preco;}
+        return soma;
+    }
+
+}
+
+class Loja{
+    private estoque:Produto[]=[];
+    carrinho = new carrinhoDeCompra();
+
+    constructor(){
+
+    }
+
+    adicionarProdutoLoja(produto: Produto){
+        this.estoque.push(produto);
+
+    }
+
+    remover(produto: Produto){
+
+        this.estoque = this.estoque.filter((item) => item.nome !== produto.nome);
+        return "produto removido do estoque da loja";
+
+    }
+
+    adicionarProdutoCarrinho(produto: Produto){
+        const produtoEncontrado = this.estoque.find(
+            item => item.nome == produto.nome
+        );
+        if (produtoEncontrado){
+            this.carrinho.adicionarProduto(produto);
+            return "Produto adicionado ao carrinho de compras";
+        }
+        else{
+            return "Produto não encontrado no estoque";
+        }
+
+    }
+
+    RemoverProdutoCarrinho(produto: Produto){
+        return this.carrinho.remover(produto);
+        //return "produto removido do carrinho de compras";
+    }
+
+    exibirProduto(produto:Produto){
+        console.log("exibir produto")
+        for (let i=0; i< this.carrinho.lista.length; i ++){
+           console.log("exibir produto : "+this.carrinho.lista[i].nome+" tem o preço de: "+this.carrinho.lista[i].preco);
+          
+      }
+}
+}
+
+
+
+
+
+
+
+
+
+const produtos1 = new Produto("Camisa", 80);
+const produtos2 = new Produto("Notbook", 6000);
+const produtos3 = new Produto("Capinha", 10);
+
+const produtosCarrinho = new Loja()
+produtosCarrinho.adicionarProdutoLoja(produtos1);
+produtosCarrinho.adicionarProdutoLoja(produtos2);
+produtosCarrinho.adicionarProdutoLoja(produtos3);
+
+console.log(produtosCarrinho.remover(produtos1));
+console.log(produtosCarrinho.adicionarProdutoCarrinho(produtos2));
+console.log(produtosCarrinho.RemoverProdutoCarrinho(produtos3));
+console.log(produtosCarrinho.exibirProduto(produtos1));
